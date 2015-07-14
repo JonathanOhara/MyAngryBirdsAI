9e82a827b30a4cba07edd262484e5dc741bc2897
Jonathan <jonathan@soc.com.br>
2015-07-14 09:31:02 -0300

    Release bat finalizada

--- Release.bat
+++ Release.bat
@@ -4,6 +4,7 @@ copy UFAngryBirdsC.jar release
 cd release
 mkdir reports
 cd reports
+
 mkdir 1
 mkdir 2
 mkdir 3
@@ -25,6 +26,69 @@ mkdir 18
 mkdir 19
 mkdir 20
 mkdir 21
+
 cd..
 cd..
-xcopy reports/1 release
+
+xcopy reports\1\shots.zip release\reports\1\
+xcopy reports\1\states.zip release\reports\1\
+
+xcopy reports\2\shots.zip release\reports\2\
+xcopy reports\2\states.zip release\reports\2\
+
+xcopy reports\3\shots.zip release\reports\3\
+xcopy reports\3\states.zip release\reports\3\
+
+xcopy reports\4\shots.zip release\reports\4\
+xcopy reports\4\states.zip release\reports\4\
+
+xcopy reports\5\shots.zip release\reports\5\
+xcopy reports\5\states.zip release\reports\5\
+
+xcopy reports\6\shots.zip release\reports\6\
+xcopy reports\6\states.zip release\reports\6\
+
+xcopy reports\7\shots.zip release\reports\7\
+xcopy reports\7\states.zip release\reports\7\
+
+xcopy reports\8\shots.zip release\reports\8\
+xcopy reports\8\states.zip release\reports\8\
+
+xcopy reports\9\shots.zip release\reports\9\
+xcopy reports\9\states.zip release\reports\9\
+
+xcopy reports\10\shots.zip release\reports\10\
+xcopy reports\10\states.zip release\reports\10\
+
+xcopy reports\11\shots.zip release\reports\11\
+xcopy reports\11\states.zip release\reports\11\
+
+xcopy reports\12\shots.zip release\reports\12\
+xcopy reports\12\states.zip release\reports\12\
+
+xcopy reports\13\shots.zip release\reports\13\
+xcopy reports\13\states.zip release\reports\13\
+
+xcopy reports\14\shots.zip release\reports\14\
+xcopy reports\14\states.zip release\reports\14\
+
+xcopy reports\15\shots.zip release\reports\15\
+xcopy reports\15\states.zip release\reports\15\
+
+xcopy reports\16\shots.zip release\reports\16\
+xcopy reports\16\states.zip release\reports\16\
+
+xcopy reports\17\shots.zip release\reports\17\
+xcopy reports\17\states.zip release\reports\17\
+
+xcopy reports\18\shots.zip release\reports\18\
+xcopy reports\18\states.zip release\reports\18\
+
+xcopy reports\19\shots.zip release\reports\19\
+xcopy reports\19\states.zip release\reports\19\
+
+xcopy reports\20\shots.zip release\reports\20\
+xcopy reports\20\states.zip release\reports\20\
+
+xcopy reports\21\shots.zip release\reports\21\
+xcopy reports\21\states.zip release\reports\21\
