915ee0920af1b56177dd779dc211ab6ea965341f
Jonathan <jonathanblacknight@gmail.com>
2015-07-14 08:15:28 -0300

    Release bat

---
+++ Release.bat
@@ -0,0 +1,30 @@
+mkdir release
+copy "RUN - Client JAR.bat" release
+copy UFAngryBirdsC.jar release
+cd release
+mkdir reports
+cd reports
+mkdir 1
+mkdir 2
+mkdir 3
+mkdir 4
+mkdir 5
+mkdir 6
+mkdir 7
+mkdir 8
+mkdir 9
+mkdir 10
+mkdir 11
+mkdir 12
+mkdir 13
+mkdir 14
+mkdir 15
+mkdir 16
+mkdir 17
+mkdir 18
+mkdir 19
+mkdir 20
+mkdir 21
+cd..
+cd..
+xcopy reports/1 release
