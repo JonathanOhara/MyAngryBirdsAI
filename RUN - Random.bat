5ded594d7295d3d276775370b8883de8a160405e
Jonathan Amazon <Administrator@172.31.34.244>
2015-07-26 21:58:23 -0300

    More reports in 10, 19, 20 levels

--- RUN - Random.bat
+++ RUN - Random.bat
@@ -1,1 +1,1 @@
-java -classpath .\bin;.\external\commons-codec-1.7.jar;.\external\Jama-1.0.2.jar;.\external\jar-in-jar-loader.zip;.\external\json-simple-1.1.1.jar;.\external\WebSocket.jar;.\external\gson-2.3.1.jar ab.main.MainEntry -level=1 -showMBR -learn=random
+java -classpath .\bin;.\external\commons-codec-1.7.jar;.\external\Jama-1.0.2.jar;.\external\jar-in-jar-loader.zip;.\external\json-simple-1.1.1.jar;.\external\WebSocket.jar;.\external\gson-2.3.1.jar ab.main.MainEntry -level=19 -learn=random
